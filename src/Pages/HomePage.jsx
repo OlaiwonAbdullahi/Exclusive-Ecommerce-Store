3ad1408e71f77cd2bd5f22143b3221870499d46c
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-13 21:16:41 +0100

    update HomePage.jsx

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Hero from "../Components/Hero";
 import MobileNavBar from "../Components/MobileNavBar";
 import NavBar from "../Components/NavBar";
 import TodayProduct from "../Components/TodayProduct";
+import Categories from "../assets/cartegory.png";
 
 const HomePage = () => {
   return (
@@ -15,6 +16,9 @@ const HomePage = () => {
         <hr className=" border-t border-text1 w-5/6" />
       </div>
       <BestSelling />
+      <div className=" w-full flex justify-center md:h-96 h-48">
+        <img src={Categories} alt="" className=" md:w-5/6 w-full" />
+      </div>
     </div>
   );
 };
