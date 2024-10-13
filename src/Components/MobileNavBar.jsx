542bd7b5bfdb6d1a9f2832d23701d2d3bc87e667
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-13 17:38:09 +0100

    update MobileNavBar.jsx

--- src/Components/MobileNavBar.jsx
+++ src/Components/MobileNavBar.jsx
@@ -1,4 +1,4 @@
-import { CiHeart, CiUser } from "react-icons/ci";
+import { CiHeart, CiMenuFries, CiUser } from "react-icons/ci";
 import SearchBar from "./SearchBar";
 import TopHeader from "./TopHeader";
 import { PiShoppingCartThin } from "react-icons/pi";
@@ -11,23 +11,34 @@ const MobileNavBar = () => {
         <div className=" font-semibold text-button text-2xl">
           Exclusive Store
         </div>
-        <div className=" ">
-          <ul className="flex gap-9 font-light">
-            <li className="border-b border-b-text1/20">Home</li>
-            <li>Contact</li>
-            <li>About</li>
-            <li>Sign Up</li>
-          </ul>
-        </div>
-        <div className=" flex gap-5 items-center">
-          <SearchBar />
-          <CiHeart className=" h-6 w-6" />
-          <PiShoppingCartThin className=" h-6 w-6" />
-          <CiUser className=" h-6 w-6" />
+        <div className="">
+          <CiMenuFries />
         </div>
       </div>
+      <Nav />
     </div>
   );
 };
 
 export default MobileNavBar;
+
+function Nav() {
+  return (
+    <div className="">
+      <div className=" ">
+        <ul className="flex gap-9 font-light">
+          <li className="border-b border-b-text1/20">Home</li>
+          <li>Contact</li>
+          <li>About</li>
+          <li>Sign Up</li>
+        </ul>
+      </div>
+      <div className=" flex gap-5 items-center">
+        <SearchBar />
+        <CiHeart className=" h-6 w-6" />
+        <PiShoppingCartThin className=" h-6 w-6" />
+        <CiUser className=" h-6 w-6" />
+      </div>
+    </div>
+  );
+}
