b67d4f00500a5c678a01237bd6ca35a801372c76
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-13 16:33:14 +0100

    update TodayProduct.jsx

--- src/Components/TodayProduct.jsx
+++ src/Components/TodayProduct.jsx
@@ -32,12 +32,12 @@ const TodayProduct = () => {
   }
 
   return (
-    <div className=" p-8 flex flex-col gap-8">
-      <h1 className=" text-secondary2 text-xl border-l-[20px] rounded-sm border-l-secondary2 p-1">
+    <div className="p-8 flex flex-col gap-8">
+      <h1 className="text-secondary2 text-xl border-l-[20px] rounded-sm border-l-secondary2 p-1">
         Today&apos;s
       </h1>
       <FlashSales />
-      <div key={todayProduct.id} className=" flex justify-around">
+      <div key={todayProduct.id} className="flex justify-around">
         {todayProduct.products.map((product) => (
           <Product key={product.id} product={product} />
         ))}
@@ -50,17 +50,22 @@ export default TodayProduct;
 
 function Product({ product }) {
   return (
-    <div className="product">
-      <div className=" bg-secondary">
+    <div className="relative product bg-white rounded-lg  p-4 flex flex-col">
+      <div className="relative bg-secondary rounded-md">
         <img
           src={product.thumbnail || "placeholder-image-url.jpg"} // Show thumbnail or placeholder if not available
           alt={product.title}
           className="w-40 h-auto"
         />
+
+        <div className="absolute top-2 left-2 bg-secondary2 text-text text-xs w-10 rounded-sm flex items-center justify-center">
+          -{product.discountPercentage}%
+        </div>
       </div>
-      <p>{product.title}</p>
-      <div className=" flex gap-3">
-        <span className="text-secondary2">
+
+      <p className="font-medium mt-2">{product.title}</p>
+      <div className="flex gap-3 mt-1">
+        <span className="text-secondary2 font-semibold">
           ${product.price.toLocaleString()}
         </span>{" "}
         <span className="text-text1 line-through">
