9245d137ad589354fa194ae68b944234847c5230
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-13 21:21:58 +0100

    some type shii

---
+++ src/Components/StarRating.jsx
@@ -0,0 +1,17 @@
+import { CiStar } from "react-icons/ci";
+import { TbStarFilled } from "react-icons/tb";
+import { FaStarHalfStroke } from "react-icons/fa6";
+
+const StarRating = () => {
+  return (
+    <div>
+      <TbStarFilled />
+      <TbStarFilled />
+      <TbStarFilled />
+      <FaStarHalfStroke />
+      <CiStar />
+    </div>
+  );
+};
+
+export default StarRating;
