55358a365a0b1a861ec8fe2ef5d3e97499e9b7c7
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-16 09:35:57 +0100

    update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -5,13 +5,14 @@ import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div className="flex flex-col md:flex-row py-2 h-screen md:h-full my-auto items-center  mb-2">
-      <div className="hidden md:block ">
+    <div className="flex flex-col md:flex-row py-2 h-screen items-center justify-center">
+      {/* Image Section */}
+      <div className="hidden md:block">
         <img src={Signupimg} alt="Signup" className="w-full h-[700px]" />
       </div>
 
       {/* Form Section */}
-      <div className="w-full md:w-1/2 flex flex-col items-center justify-center">
+      <div className="w-full md:w-1/2 flex flex-col items-center justify-center h-full">
         <form
           action=""
           className="flex flex-col gap-4 justify-center md:w-2/3 w-full px-4 md:px-8 lg:px-16"
@@ -41,7 +42,7 @@ const Signup = () => {
 
           <div className="flex flex-col gap-4 mt-2">
             <Button>Create Account</Button>
-            <button className=" hover:bg-text1/10 text-lg items-center justify-center gap-2 flex p-2 rounded bg-white border border-text2">
+            <button className="hover:bg-text1/10 text-lg items-center justify-center gap-2 flex p-2 rounded bg-white border border-text2">
               <FcGoogle className="h-6 w-6" />
               Sign Up with Google
             </button>
@@ -49,7 +50,7 @@ const Signup = () => {
         </form>
         <p className="mt-2 text-center">
           Already have an account?{" "}
-          <span className="underline  cursor-pointer">
+          <span className="underline cursor-pointer">
             <Link to="/login">Log in</Link>
           </span>
         </p>
