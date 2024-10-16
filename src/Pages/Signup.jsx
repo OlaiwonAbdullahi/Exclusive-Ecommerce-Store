cc73bbd40f09a8bd64fec386cf883b1cc76d03d7
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-16 08:33:54 +0100

    update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -4,39 +4,55 @@ import Signupimg from "../assets/Signup.svg";
 
 const Signup = () => {
   return (
-    <div className=" flex py-2">
-      <div className="">
-        <img src={Signupimg} alt="" className=" w-3/4" />
+    <div className="flex flex-col md:flex-row py-2 items-center h-screen">
+      {/* Image Section */}
+      <div className="hidden md:block">
+        <img src={Signupimg} alt="Signup" className="w-3/4 h-[700px]" />
       </div>
-      <div className=" ">
-        <form action="" className=" flex flex-col gap-2">
-          <div className="">
-            <h2 className=" text-3xl font-medium">Create an account</h2>
-            <span className=" text-base">Enter our Details Below</span>
+
+      {/* Form Section */}
+      <div className="w-full md:w-1/2 flex flex-col items-center justify-center">
+        <form
+          action=""
+          className="flex flex-col gap-4 justify-center w-2/3 px-4 md:px-8 lg:px-16"
+        >
+          <div className="text-left">
+            <h2 className="text-3xl font-medium">Create an account</h2>
+            <span className="text-base">Enter your details below</span>
           </div>
-          <div className=" flex flex-col gap-2">
+
+          <div className="flex flex-col gap-3">
             <input
               type="text"
               placeholder="Name"
-              className=" placeholder:text-text1 text-text1 border-b border-b-text1 p-2 focus:outline-none"
+              className="placeholder:text-text1 text-text1 border-b border-b-text1 p-2 focus:outline-none"
             />
             <input
               type="text"
               placeholder="Email or Phone Number"
-              className=" placeholder:text-text1 text-text1 border-b border-b-text1 p-2 focus:outline-none"
+              className="placeholder:text-text1 text-text1 border-b border-b-text1 p-2 focus:outline-none"
             />
             <input
               type="password"
               placeholder="Password"
-              className=" placeholder:text-text1 text-text1 border-b border-b-text1 p-2 focus:outline-none"
+              className="placeholder:text-text1 text-text1 border-b border-b-text1 p-2 focus:outline-none"
             />
           </div>
-          <Button>Create Account</Button>
-          <button className=" rounded bg-white border border-text2">
-            <FcGoogle />
-            Sign Up with Google
-          </button>
+
+          <div className="flex flex-col gap-4 mt-2">
+            <Button>Create Account</Button>
+            <button className="text-lg items-center justify-center gap-2 flex p-2 rounded bg-white border border-text2">
+              <FcGoogle className="h-6 w-6" />
+              Sign Up with Google
+            </button>
+          </div>
         </form>
+        <p className="mt-2 text-center">
+          Already have an account?{" "}
+          <span className="underline text-secondary cursor-pointer">
+            Log in
+          </span>
+        </p>
       </div>
     </div>
   );
