54f8786edfeb57e6fc31457453ac67a25736f16c
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-16 17:41:15 +0100

    update Contact.jsx

---
+++ src/Components/ContactCard.jsx
@@ -0,0 +1,14 @@
+import { FaPhoneSquareAlt } from "react-icons/fa";
+const ContactCard = () => {
+  return (
+    <div>
+      <div className="">
+        <div className="">
+          <FaPhoneSquareAlt className=" rounded-full text-secondary2 h-6 w-6 " />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactCard;
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,11 @@
+import ContactCard from "../Components/ContactCard";
 import Path from "../Components/Path";
 
 const Contact = () => {
   return (
     <div>
       <Path>Contact</Path>
+      <ContactCard />
     </div>
   );
 };
