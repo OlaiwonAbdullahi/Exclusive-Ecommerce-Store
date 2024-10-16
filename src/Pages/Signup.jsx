cdcaa156189c731adf61cd96b02762d86ddccf69
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-16 08:21:00 +0100

    update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -8,14 +8,29 @@ const Signup = () => {
       <div className="">
         <img src={Signupimg} alt="" className=" w-3/4" />
       </div>
-      <div className="">
-        <form action="">
-          <h2>Create an account</h2>
-          <span>enter tour details below</span>
-
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Email or Phone Number" />
-          <input type="password" placeholder="Password" />
+      <div className=" ">
+        <form action="" className=" flex flex-col gap-2">
+          <div className="">
+            <h2 className=" text-3xl font-medium">Create an account</h2>
+            <span className=" text-base">Enter our Details Below</span>
+          </div>
+          <div className=" flex flex-col gap-2">
+            <input
+              type="text"
+              placeholder="Name"
+              className=" placeholder:text-text1 text-text1 border-b border-b-text1 p-2 focus:outline-none"
+            />
+            <input
+              type="text"
+              placeholder="Email or Phone Number"
+              className=" placeholder:text-text1 text-text1 border-b border-b-text1 p-2 focus:outline-none"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className=" placeholder:text-text1 text-text1 border-b border-b-text1 p-2 focus:outline-none"
+            />
+          </div>
           <Button>Create Account</Button>
           <button className=" rounded bg-white border border-text2">
             <FcGoogle />
