b7475772d94573c77ef1178e42d5427c1927f38a
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-18 13:41:05 +0100

    update Cart.jsx

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -4,12 +4,15 @@ const Cart = () => {
   return (
     <div>
       <Path>Cart</Path>
-      <div className="">
-        <div className=" text-lg">
+      <div className=" w-3/4 flex flex-col justify-center items-center mx-auto">
+        <div className=" text-lg p-2 flex justify-around  border border-text1/10 shadow rounded md:w-3/4 w-full">
           <span>Product</span>
           <span>Price</span>
           <span>Quantity</span>
           <span>SubTotal</span>
+        </div>
+        <div className="">
+          <div className=""></div>
         </div>
       </div>
     </div>
