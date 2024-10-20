0cc77fa61ca906ca69aa2cc2e0b94b4fde169b46
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-20 21:02:57 +0100

    build(deps): update package-lock.json, package.json and CheckOut.jsx

--- src/Pages/CheckOut.jsx
+++ src/Pages/CheckOut.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import { PaystackButton } from "react-paystack";
 import Button from "../Components/Button";
 
 const CheckOut = ({ setOpenCheckout, openCheckout, total }) => {
   const publicKey = "pk_test_9f301a75ad2d5aa4e917d824f306395614690196";
+
+  // States for form fields
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [address, setAddress] = useState("");
+  const [floor, setFloor] = useState("");
+  const [town, setTown] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const amountInKobo = total * 1700 * 100;
+
+  const componentProps = {
+    email,
+    amount: amountInKobo,
+    metadata: {
+      name,
+      phoneNumber,
+    },
+    publicKey,
+    text: "Pay",
+    onSuccess: () => alert("Thank you for using our platform"),
+    onClose: () => alert("Are you sure you want to close?"),
+  };
+
   return (
     <div className="relative bg-text text-text1 overflow-y-auto w-2/3 max-w-md rounded-lg p-6 shadow-lg max-h-[100%] pt-52 scrollbar-hide items-center gap-2 flex flex-col justify-center">
       <div className="flex flex-col gap-2">
@@ -14,12 +41,14 @@ const CheckOut = ({ setOpenCheckout, openCheckout, total }) => {
             &times;
           </button>
         </div>
-        <form action="">
+        <form>
           <div className="flex flex-col">
             <label htmlFor="name">First Name</label>
             <input
               type="text"
               id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className="bg-text1/10 w-full p-2 focus:outline-none text-sm rounded text-text1"
             />
           </div>
@@ -28,6 +57,8 @@ const CheckOut = ({ setOpenCheckout, openCheckout, total }) => {
             <input
               type="text"
               id="company"
+              value={company}
+              onChange={(e) => setCompany(e.target.value)}
               className="bg-text1/10 w-full p-2 focus:outline-none text-sm rounded text-text1"
             />
           </div>
@@ -38,6 +69,8 @@ const CheckOut = ({ setOpenCheckout, openCheckout, total }) => {
             <input
               type="text"
               id="address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
               required
               className="bg-text1/10 w-full p-2 focus:outline-none text-sm rounded text-text1"
             />
@@ -47,6 +80,8 @@ const CheckOut = ({ setOpenCheckout, openCheckout, total }) => {
             <input
               type="text"
               id="floor"
+              value={floor}
+              onChange={(e) => setFloor(e.target.value)}
               className="bg-text1/10 w-full p-2 focus:outline-none text-sm rounded text-text1"
             />
           </div>
@@ -57,6 +92,8 @@ const CheckOut = ({ setOpenCheckout, openCheckout, total }) => {
             <input
               type="text"
               id="town"
+              value={town}
+              onChange={(e) => setTown(e.target.value)}
               required
               className="bg-text1/10 w-full p-2 focus:outline-none text-sm rounded text-text1"
             />
@@ -68,6 +105,8 @@ const CheckOut = ({ setOpenCheckout, openCheckout, total }) => {
             <input
               type="number"
               id="phone"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
               required
               className="bg-text1/10 w-full p-2 focus:outline-none text-sm rounded text-text1"
             />
@@ -79,20 +118,22 @@ const CheckOut = ({ setOpenCheckout, openCheckout, total }) => {
             <input
               type="email"
               id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
               className="bg-text1/10 w-full p-2 focus:outline-none text-sm rounded text-text1"
             />
           </div>
-          <div className="">
+          <div>
             <input type="checkbox" className="accent-secondary2 accent-sm" />
             <span>Save this information for faster check-out next time</span>
           </div>
         </form>
       </div>
       <div className="p-5">
-        <div className="flex justify-around">
-          <div className="shadow rounded bg-white p-3 flex flex-col gap-2">
-            <div className=" text-secondary2 text-2xl">Total</div>
+        <div className="flex">
+          <div className="shadow rounded bg-white p-3 w-full flex flex-col gap-2">
+            <div className="text-secondary2 text-2xl">Total</div>
             <div className="flex justify-between border-b border-b-text1">
               <span>Subtotal:</span>
               <span>${total.toFixed(2)}</span>
@@ -105,8 +146,11 @@ const CheckOut = ({ setOpenCheckout, openCheckout, total }) => {
               <span>Total:</span>
               <span>${total.toFixed(2)}</span>
             </div>
-            <div className="">
-              <Button width={200}>Pay</Button>
+            <div>
+              <PaystackButton
+                {...componentProps}
+                className=" bg-secondary2 h-10 text-text rounded hover:bg-hoverBtn1 w-full"
+              />
             </div>
           </div>
         </div>
