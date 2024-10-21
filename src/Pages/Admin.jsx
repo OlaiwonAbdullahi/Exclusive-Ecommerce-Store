b799022f3e14dca09c70744e1b773adc9ae86bfc
Abdullahi Olaiwon <olaiwonabdullahi@gmail.com>
2024-10-21 21:43:22 +0100

    update Admin.jsx

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -7,12 +7,16 @@ import { CiGrid42 } from "react-icons/ci";
 
 const Admin = () => {
   const [active, setActive] = useState("Home");
+  const [openSidebar, setOpenSidebar] = useState(false);
   return (
     <div className="flex bg-[#F8F8F8] min-h-screen">
       <Sidebar className="w-1/4" active={active} setActive={setActive} />
-      <div className="flex-grow p-4">
+      <div
+        className="flex-grow p-4"
+        onClick={() => setOpenSidebar(!openSidebar)}
+      >
         <button className="">
-          <CiGrid42 />
+          <CiGrid42 className=" h-5 w-5" />
         </button>
         <DashboardHeader active={active} setActive={setActive} />
         <DashBoardStats />
